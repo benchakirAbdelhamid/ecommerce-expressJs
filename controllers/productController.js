241e04a974813230a135df1f4dfe7abe30e3b8e8
hamid <benchakirhamid@gmail.com>
2023-11-29 19:24:57 +0100

    add package joi becouse managment validate data product

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,13 +1,14 @@
 const Product = require("../models/product");
+const Joi = require("joi");
 
 exports.createProduct = async (req, res) => {
   try {
     // console.log(req.file.size) // 1mb =1000000 == 10^6
     // console.log(Math.pow(10 , 6))
-    if(req.file.size > Math.pow(10 , 6)){
+    if (req.file.size > Math.pow(10, 6)) {
       return res.status(400).json({
-        error : 'Image should be less than 1mb in size'
-      })
+        error: "Image should be less than 1mb in size",
+      });
     }
     const product = new Product({
       name: req.body.name,
@@ -20,14 +21,27 @@ exports.createProduct = async (req, res) => {
       },
       category: req.body.category,
     });
-    await product.save();
-    res
-      .status(201)
-      .json({
-        success: true,
-        message: "Product created successfully",
-        product,
+    // validator joi
+    const schema = Joi.object({
+      name: Joi.string().required(),
+      price: Joi.required(),
+      description: Joi.required(),
+      quantity: Joi.number().required(),
+      category: Joi.required(),
+    });
+    const { error } = schema.validate(req.body);
+    if (error) {
+      return res.status(400).json({
+        error: error.details[0].message
       });
+    }
+
+    await product.save();
+    res.status(201).json({
+      success: true,
+      message: "Product created successfully",
+      product,
+    });
   } catch (err) {
     res.status(400).json({
       success: false,
@@ -35,3 +49,24 @@ exports.createProduct = async (req, res) => {
     });
   }
 };
+
+
+exports.productById = (req , res , next , id)=>{
+  Product.findById(id).exec((err , product)=>{
+    if(err || !product){
+      return res.status(404).json({
+        error : "product not found !"
+      })
+    }
+    req.product = product;
+    next()
+  })
+}
+
+
+exports.showProduct = (req ,res)=>{
+  req.product.photo = undefined
+  res.json({
+    product
+  })
+}
--- routes/products.js
+++ routes/products.js
@@ -1,16 +1,24 @@
-const express = require('express')
-const router = express.Router()
-const { userById } = require('../middlewares/user')
-const { createProduct } = require('../controllers/productController')
+const express = require("express");
+const { userById } = require("../middlewares/user");
+const { createProduct, productById, showProduct } = require("../controllers/productController");
+const { requireSignIn, isAuth, isAdmin } = require("../middlewares/auth");
+const upload = require("../middlewares/multer");
 
-const {requireSignIn , isAuth, isAdmin} = require('../middlewares/auth')
-
-const upload = require('../middlewares/multer')
+const router = express.Router();
 
 
+router.get('/:productId' , showProduct)
 
 // router.post('/create' ,upload.single('photo'), createProduct)
-router.post('/create/:userId',[ requireSignIn , isAuth , isAdmin ],upload.single('photo'), createProduct)
-router.param('userId',userById)
+router.post(
+  "/create/:userId",
+  [requireSignIn, isAuth, isAdmin],
+  upload.single("photo"),
+  createProduct
+);
 
-module.exports = router
+
+router.param("userId", userById);
+router.param("productId", productById);
+
+module.exports = router;
