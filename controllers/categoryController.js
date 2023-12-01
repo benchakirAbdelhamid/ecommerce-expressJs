89bc5690bada6aee6dde95172cc4d3014d072f04
hamid <benchakirhamid@gmail.com>
2023-12-01 01:35:23 +0100

    add new controllers update and delete and get all category

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,18 +1,82 @@
-const Category = require('../models/category')
+const Category = require("../models/category");
 
-
-exports.createCategory = async(req , res)=>{
+exports.allCategories = async (req, res)=>{
     try {
-        const category = new Category(req.body)
-        await category.save();
+        const category = await Category.find();
+        if (!category) {
+          return res.status(404).json({ error: "category not found" });
+        }
+       res.json({category})
+      } catch (error) {
+        res.status(404).json({ error: error.message });
+      }
+}
+
+exports.createCategory = async (req, res) => {
+  try {
+    const category = new Category(req.body);
+    await category.save();
+    res.json({
+      message: "Successfully create category",
+      category: category,
+    });
+  } catch (error) {
+    return res.status(400).json({
+      error: "bad Request",
+    });
+  }
+};
+
+exports.categoryId = async (req, res, next, id) => {
+  try {
+    const category = await Category.findById(id);
+    if (!category) {
+      return res.status(404).json({ error: "category not found" });
+    }
+    req.category = category;
+    next();
+  } catch (error) {
+    res.status(404).json({ error: error.message });
+  }
+};
+
+exports.showCategory = async (req, res) => {
+  //   let { categoryId } = req.params;
+  try {
+    let category = req.category;
+    res.json({ category });
+  } catch (error) {
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+exports.updateCategory = async (req, res)=>{
+    try {
+        let category = req.category
+        category.name = req.body.name
+        await category.save()
         res.json({
-            message : "Successfully create category",
-            category : category
+            category,
+            message : 'Category updated'
         })
+        
     } catch (error) {
         return res.status(400).json({
-            error : "bad Request"
+            error : "bad request !"
         })
     }
-
+}
+exports.deleteCategory = async (req, res)=>{
+    try {
+        let category = req.category
+        await category.deleteOne()
+        res.status(200).json({
+            message : 'Category deleted successfully'
+        })
+        
+    } catch (error) {
+        return res.status(404).json({
+            error : "category not found !"
+        })
+    }
 }
--- routes/categories.js
+++ routes/categories.js
@@ -1,12 +1,19 @@
 const express = require('express')
-const { createCategory } = require('../controllers/categoryController')
+const { createCategory,categoryId,allCategories,showCategory,updateCategory,deleteCategory } = require('../controllers/categoryController')
 const { userById } = require('../middlewares/user')
 const router = express.Router()
 
 const {requireSignIn , isAuth, isAdmin} = require('../middlewares/auth')
 
 
+router.get('/' , allCategories)
+router.get('/:categoryId' , showCategory)
 router.post('/create/:userId',[ requireSignIn , isAuth , isAdmin ] , createCategory)
+router.put('/:categoryId/:userId',[ requireSignIn , isAuth , isAdmin ] , updateCategory)
+router.delete('/:categoryId/:userId',[ requireSignIn , isAuth , isAdmin ] , deleteCategory)
+
+
 router.param('userId',userById)
+router.param('categoryId',categoryId)
 
 module.exports = router
